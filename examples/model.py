90d1ffcdcb4c40ab0df3f9b3947e23936f2e444b
Ratul Maharaj <ratulmaharaj@gmail.com>
2022-08-15 01:52:17 +0200

    Made model work

--- examples/model.py
+++ examples/model.py
@@ -1,63 +1,47 @@
 # `pip install -e .` to install the library
 # import the library
-from divination import (
-    CashFlow,
-    DiscountFactors,
-    Model,
-    StaticFlow,
-    set_precision,
-)
+from divination import CashFlow, DiscountFactors, Model, StaticFlow
 
 # Create new model instance
 model = Model()
 
-# Set rounding level
-# returns precision but can also call get_precision
-rounding = set_precision(4)
-
 # Add a premium component
-# The 'formula' can be used to adjust the values of the cashflows over time
-prem = CashFlow(
-    input_array=[100],
-    formula=lambda prev: prev * 1.05,
-    label="premium",
+model.add_component(
+    CashFlow(
+        input_array=[100], formula=lambda prev: prev * 1.05, label="premium"
+    )
 )
-# Register the component
-model.add_component(prem)
 
 # Add a cover component
-cover = CashFlow(
-    label="cover",
-    input_array=[1_000_000],
-)
-# Register the component
-model.add_component(cover)
+model.add_component(CashFlow(label="cover", input_array=[1_000_000]))
 
 # Add an expense component
-exp = StaticFlow(
-    label="expense",
-    input_array=[10 for _ in range(0, 5)],
+model.add_component(
+    StaticFlow(
+        input_array=[10, 10, 10, 10, 10],
+        label="expense",
+    )
 )
-# Register the component
-model.add_component(exp)
 
 # Add discounting component
-disc = DiscountFactors(interest_rate=0.05, label="V")
-# Register the component
-model.add_component(disc)
-
-# TODO: define relationships between variables somehow?
-# Allow the creation of derived cashflow vectors using labels
-# (premium - cover - expense ) * discounting
-
-# Add derived component - registers automatically
-# model.derive_component(label="reserve", expression="premium - cover")
+model.add_component(DiscountFactors(interest_rate=0.05, label="V"))
 
 # Project cashflows over term
-results = model.project(term=10)
+# Results return a pandas df object
+df = model.project(term=10)
+
+# Perform linear combination style manipulations
+# Discounting the components
+components = ["premium", "cover", "expense"]
+for component in components:
+    df[f"V_{component}"] = df[component] * df["V"]
+
+
+# Define reserving relationship
+df["Reserve"] = df["V_cover"] + df["V_expense"] - df["V_premium"]
 
 # Results get returned as a pandas dataframe
-print(results)
+print(df)
 
 # TODO: How this 'model.py' will be applied to modelpoints is still TBD
 # TODO: Inputs to many of the cashflow vectors would need to come from the data
