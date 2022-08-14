563c467efceca2a9826fa0b685fb1cf7022b0313
Ratul Maharaj <ratulmaharaj@gmail.com>
2022-08-14 17:35:31 +0200

    Added simple example

--- divination/__init__.py
+++ divination/__init__.py
@@ -1,1 +1,4 @@
 # from .module import function, atrribute
+from .core.flows import CashFlow, StaticFlow
+from .core.model import Model
+from .core.precision import get_precision, set_precision
---
+++ examples/simple_model.py
@@ -0,0 +1,39 @@
+# import the library
+import divination as dv
+
+# Create new model instance
+model = dv.Model()
+
+# Set rounding level
+# returns precision but can also call get_precision
+rounding = dv.set_precision(4)
+
+# Add a premium component
+prem = dv.CashFlow(
+    label="premium",
+    formula=lambda x: x * 1.05,
+    initial_data=[100],
+)
+model.add_component(prem)
+
+# Add a cover component
+cover = dv.CashFlow(
+    label="cover",
+    initial_data=[1_000_000],
+)
+model.add_component(cover)
+
+# Add an expense component
+exp = dv.StaticFlow(
+    label="expense",
+    data=[10 for _ in range(0, 5)],
+)
+model.add_component(exp)
+
+# define relationships between variables somehow?
+
+# Project cashflows over term
+results = model.project(term=10)
+
+# Results get returned as a pandas dataframe
+print(results)
