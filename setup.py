bd6e575f4507b58636cd30bf0526fb1e897cdb31
Ratul Maharaj <ratulmaharaj@gmail.com>
2022-08-15 02:05:43 +0200

    Updated semver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="divination",
-    version="0.0.0",
+    version="0.1.0",
     author="Ratul Maharaj",
     author_email="ratulmaharaj@gmail.com",
     description="A framework for Actuarial modelling.",
