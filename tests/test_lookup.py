aa3450b4ad24ba2db11cc83990ad48b4763c94c9
Ratul Maharaj <ratulmaharaj@gmail.com>
2022-08-22 02:55:05 +0200

    Added engine and lookup tests

---
+++ tests/test_lookup.py
@@ -0,0 +1,46 @@
+import pytest
+from src.divinate import TableLookup
+
+
+def test_tablelookup():
+    # Create a new instance
+    tbl = TableLookup(
+        table_name="test.csv",
+        lookup_on="age",
+        default_value=0,
+        label="test",
+        keep_column="test",
+    )
+    assert isinstance(tbl, TableLookup)
+
+
+def test_missing_lookup_on():
+    with pytest.raises(ValueError):
+        TableLookup(
+            table_name="test.csv",
+            default_value=0,
+            label="test",
+            keep_column="test",
+        )
+
+
+def test_missing_keep_col():
+    with pytest.raises(ValueError):
+        TableLookup(
+            table_name="test.csv",
+            default_value=0,
+            label="test",
+            lookup_on=["test"],
+        )
+
+
+@pytest.mark.parametrize("default_value", [0, 5.0, "Test"])
+def test_defaults(default_value):
+    tbl = TableLookup(
+        table_name="test.csv",
+        default_value=default_value,
+        label="test",
+        lookup_on=["test"],
+        keep_column="test",
+    )
+    assert tbl.default_value == default_value
--- tests/test_run.py
+++ tests/test_run.py
@@ -2,18 +2,57 @@ import pytest
 from src.divinate import RunConfig, dataclass
 
 
-def test_create_run_settings():
-    @dataclass
-    class ModelPoint:
-        test: str
+@dataclass
+class ModelPoint:
+    test: str
 
-    def handler(modelpoint):
-        pass
 
+def handler(modelpoint):
+    pass
+
+
+def test_create_instance():
+    rc = RunConfig(
+        handler=handler,
+        modelpoint_definition=ModelPoint,
+        table_location=None,
+        modelpoint_file=None,
+        results_location=None,
+        label="test_config",
+    )
+    assert isinstance(rc, RunConfig)
+
+
+def test_create_multiple_instances():
+    rc2 = RunConfig(
+        handler=handler,
+        modelpoint_definition=ModelPoint,
+        table_location=None,
+        modelpoint_file=None,
+        results_location=None,
+        label="test_config_2",
+    )
+    assert isinstance(rc2, RunConfig)
+    # test that instances get added to the instances list
+    assert len(RunConfig.instances) == 2
+
+
+def test_missing_modelpoint_file():
     with pytest.raises(FileNotFoundError):
         RunConfig(
             handler,
             ModelPoint,
             modelpoint_file="/path/to/file/that/does/not/exist.csv",
+            results_location=None,
+        )
+
+
+def test_missing_table_location():
+    with pytest.raises(FileNotFoundError):
+        RunConfig(
+            handler=handler,
+            modelpoint_definition=ModelPoint,
+            table_location="/path/to/fictional/table/location",
+            modelpoint_file=None,
             results_location=None,
         )
